661ff5900e1b529e78f093673f1e193cfe3bf26d
ziadnabil <ziadmoh2525@gmail.com>
2021-09-26 22:48:03 +0200

    nested components

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -3,19 +3,28 @@ import ReactDom from "react-dom";
 
 
 // first component
-// function Greeting() {
-//   return (
-//     <div>
-//       <h1>hello world</h1>
-//     </div>);
-// }
+function Greeting() {
+  return (
+    <div>
+      <Title />
+      <Paragraph />
+    </div>
+  );
+}
+
+const Title = () => <h2>Solo Leveling</h2>;
+const Paragraph = () => {
+  return <p> Let the hunt begin </p>;
+}
 
 // using arrow function
 // method looks for 3 thinks : what element we want to return
 //                               props object => we will pass an empty object
 //                               what is going to rendered inside that element
-const Greeting = () => {
-  return React.createElement('div',{}, React.createElement('h1',{}, 'hello world'))
-}
+// const Greeting = () => {
+//   return React.createElement('div',{}, React.createElement('h1',{}, 'hello world'))
+// }
 // passing what we are going to render and where to render it.
+
+
 ReactDom.render(<Greeting />, document.getElementById('root'));
